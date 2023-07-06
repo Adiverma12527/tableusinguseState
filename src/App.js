3fff9bf290018c414d775e80b2c1a01f8fb955c1
aditya verma <adityavermasuper.12527@gmail.com>
2023-07-06 10:31:36 +0530

    createtableusinguseState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleInputChange = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const generateTable = () => {
+    const tableRows = [];
+
+    for (let i = 1; i <= number; i++) {
+      tableRows.push(
+        <tr key={i}>
+          <td>{i}</td>
+          <td>{i * 2}</td>
+          <td>{i * 3}</td>
+
+        </tr>
+      );
+    }
+
+    return tableRows;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Enter a number:
+        <input type="number" value={number} onChange={handleInputChange} />
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>Double</th>
+            <th>Triple</th>
+          </tr>
+        </thead>
+        <tbody>{generateTable()}</tbody>
+      </table>
     </div>
   );
 }
